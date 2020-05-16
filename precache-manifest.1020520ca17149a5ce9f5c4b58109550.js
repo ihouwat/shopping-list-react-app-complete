fb7badebbcf892a3b0630f0ea99d6f7a2c1af313
ihouwat <houwat@gmail.com>
2020-05-16 15:05:50 -0400

    Updates

---
+++ precache-manifest.1020520ca17149a5ce9f5c4b58109550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53974c1d6764323e09732a7114f3c5d9",
+    "url": "/shopping-list-app-live/index.html"
+  },
+  {
+    "revision": "2ac49f6d6aa0a4138854",
+    "url": "/shopping-list-app-live/static/css/main.216097bf.chunk.css"
+  },
+  {
+    "revision": "8ff677d9a5cb5af8d6d2",
+    "url": "/shopping-list-app-live/static/js/2.8ef45119.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/shopping-list-app-live/static/js/2.8ef45119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac49f6d6aa0a4138854",
+    "url": "/shopping-list-app-live/static/js/main.12413128.chunk.js"
+  },
+  {
+    "revision": "d36bb5e877bc0c544cc5",
+    "url": "/shopping-list-app-live/static/js/runtime-main.6d3ef3c4.js"
+  }
+]);
